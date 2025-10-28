68779bc2170a9cd70a7f28ddc5067e29d8700a5b
ramanan1998 <ramananr2698095@gmail.com>
2025-10-28 16:59:07 +0530

    removed buttons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import LoginButton from "./components/LoginButton";
-import LogoutButton from "./components/LogoutButton";
-import Profile from "./components/Profile";
 
 function App() {
 
   return (
     <main className="column">
       <h1>Auth0 Login</h1>
-      <>
-        <LoginButton />
-      </>
+      <LoginButton />
     </main>
   );
 }
