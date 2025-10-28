08d2cb3a493c05c0d8aea4ccdfc52b514fec2805
ramanan1998 <ramananr2698095@gmail.com>
2025-10-28 17:04:46 +0530

    removed auth provider

--- src/index.js
+++ src/index.js
@@ -2,18 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Auth0Provider } from '@auth0/auth0-react';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      redirectUri={window.location.origin}
-    > */}
-      <App />
-    {/* </Auth0Provider> */}
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
